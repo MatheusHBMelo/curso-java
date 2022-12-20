d70697885aeb51033651a991fcf1f45d9a4c1432
MatheusHBMelo <matheushbmelo@gmail.com>
2022-12-20 09:26:25 -0300

    Versao 01 - contato

--- src/composicaoDeClasses/contatos/App.java
+++ src/composicaoDeClasses/contatos/App.java
@@ -1,7 +1,20 @@
 package composicaoDeClasses.contatos;
 
+import composicaoDeClasses.contatos.entities.Contato;
+
 public class App {
     public static void main(String[] args) {
-        
+        // Instanciando um objeto do tipo contato
+        Contato contato = new Contato();
+
+        // Atribuindo valores aos atributos pelos métodos setters
+        contato.setNome("Matheus Henrique");
+        contato.setEndereco("Recife, PE - Brasil");
+        contato.setTelefone("81 99999-0000");
+
+        // Imprimindo os valores dos atributos pelos métodos getters
+        System.out.println("Nome: " + contato.getNome());
+        System.out.println("Endereço: " + contato.getEndereco());
+        System.out.println("Telefone: " + contato.getTelefone());
     }
 }
