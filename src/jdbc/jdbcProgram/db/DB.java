5859ed08219f17dca83308a96098f3005ddc50dc
MatheusHBMelo <matheushbmelo@gmail.com>
2023-04-26 17:43:13 -0300

    Estudo API jdbc

--- src/jdbc/jdbcProgram/db/DB.java
+++ src/jdbc/jdbcProgram/db/DB.java
@@ -6,6 +6,7 @@ import java.sql.*;
 import java.util.Properties;
 
 public class DB {
+    // Método para ler as propriedades do arquivo db.properties
     private static Properties loadProperties(){
         try (FileInputStream fs = new FileInputStream("db.properties")){
             Properties prop = new Properties();
@@ -16,8 +17,10 @@ public class DB {
         }
     }
 
+    // Variavel estática para representar a conexão com o banco de dados
     private static Connection conn = null;
 
+    // Métodos para abrir uma conexão com o banco de dados
     public static Connection getConnection(){
         if (conn == null){
             try {
@@ -31,6 +34,7 @@ public class DB {
         return conn;
     }
 
+    // Métodos para fechar uma conexão com o banco de dados
     public static void closeConnection(){
         if (conn != null){
             try {
@@ -41,6 +45,7 @@ public class DB {
         }
     }
 
+    // Método para fechar uma Statement
     public static void closeStatement(Statement st){
         if (st != null){
             try {
@@ -51,6 +56,7 @@ public class DB {
         }
     }
 
+    // Método para fechar um ResultSet
     public static void closeResultSet(ResultSet rs){
         if (rs != null){
             try {
